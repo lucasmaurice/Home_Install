31026f53e7b72c929e52be43782907f1abe7d2c8
Lucas Maurice <lucas.maurice@outlook.com>
2016-12-06 17:57:27 -0500

    update waiting time

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ sudo cp -r homebridge/*
 echo "Launching  Homebridge..."
 sudo systemctl start homebridge
 echo "Waiting for launch..."
-sleep 10
+sleep 3
 echo "Launch logs :"
 sudo journalctl -u homebridge --since "1 minute ago"
