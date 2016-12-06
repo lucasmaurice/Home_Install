7dd75d51b80d347447b4ee6033fe004a1a09e754
Lucas Maurice <lucas.maurice@outlook.com>
2016-12-06 16:55:19 -0500

    Loading old work

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#wget https://nodejs.org/dist/v4.3.2/node-v4.3.2-linux-armv6l.tar.gz
+#tar -xvf node-v4.3.2-linux-armv6l.tar.gz 
+#cd node-v4.3.2-linux-armv6l
+#sudo cp -R * /usr/local/
+
+#sudo apt-get --assume-yes install libavahi-compat-libdnssd-dev
+
+#sudo npm install -g --unsafe-perm homebridge hap-nodejs node-gyp
+#cd /usr/local/lib/node_modules/homebridge/
+#sudo npm install --unsafe-perm bignum
+#cd /usr/local/lib/node_modules/hap-nodejs/node_modules/mdns
+#sudo node-gyp BUILDTYPE=Release rebuild
+
+#sudo cp service/homebridge /etc/default
+#sudo cp service/homebridge.service /etc/systemd/system
+
+#sudo useradd --system homebridge
+#sudo mkdir /var/homebridge
+
+#sudo timedatectl set-timezone America/Toronto
+
+#sudo systemctl daemon-reload
+#sudo systemctl enable homebridge
+#sudo systemctl start homebridge
+
+#sudo npm install serialport --unsafe-perm --build-from-source
+#sudo npm install -g homebridge-marantz-rs232
